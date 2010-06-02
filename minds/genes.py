df3c8dbb537407907033abada3a5a004029b5be8
Scott Wolchok <scott@wolchok.org>
2010-06-02 15:57:43 -0400

    use reindent.py to enforce 4-space indent

--- minds/genes.py
+++ minds/genes.py
@@ -7,45 +7,45 @@ background in that area.
 import random
 
 class Gene(object):
-  def __init__(self, parent):
-    '''Clone this gene from the parent gene.'''
-    self.val = parent.val
+    def __init__(self, parent):
+        '''Clone this gene from the parent gene.'''
+        self.val = parent.val
 
-  def spawn(self):
-    '''Copy this gene, introducing mutations probabilistically.'''
-    new = self.__class__(self)
-    new.mutate()
-    return new
+    def spawn(self):
+        '''Copy this gene, introducing mutations probabilistically.'''
+        new = self.__class__(self)
+        new.mutate()
+        return new
 
-  def mutate(self):
-    perturb = self.gen_perturb()
-    val = self.val + perturb
-    self.val = min(max(val, self.min_cap), self.max_cap)
+    def mutate(self):
+        perturb = self.gen_perturb()
+        val = self.val + perturb
+        self.val = min(max(val, self.min_cap), self.max_cap)
 
 
 def make_normally_perturbed_gene(sigma, minc=0, maxc=1):
-  class NormallyPerturbedGene(Gene):
-    min_cap = minc
-    max_cap = maxc
-    def gen_perturb(self):
-      return random.gauss(0, sigma)
-  return NormallyPerturbedGene
+    class NormallyPerturbedGene(Gene):
+        min_cap = minc
+        max_cap = maxc
+        def gen_perturb(self):
+            return random.gauss(0, sigma)
+    return NormallyPerturbedGene
 
 
 def make_drastic_mutation_gene(pr):
-  '''Gene representing incompatible categories.'''
-  class DrasticMutationGene(Gene):
-    min_cap = 0
-    max_cap = 100
-    def gen_perturb(self):
-      if random.random() < pr:
-        return 1 if random.random() < 0.5 else -1
-      else:
-        return 0
-  return DrasticMutationGene
+    '''Gene representing incompatible categories.'''
+    class DrasticMutationGene(Gene):
+        min_cap = 0
+        max_cap = 100
+        def gen_perturb(self):
+            if random.random() < pr:
+                return 1 if random.random() < 0.5 else -1
+            else:
+                return 0
+    return DrasticMutationGene
 
 
 class InitializerGene(object):
-  '''A fake gene, used to initialize things.'''
-  def __init__(self, val):
-    self.val = val
+    '''A fake gene, used to initialize things.'''
+    def __init__(self, val):
+        self.val = val
--- minds/mind1.py
+++ minds/mind1.py
@@ -1,48 +1,48 @@
 import random,cells
 import math
 class AgentMind:
-  def __init__(self, junk):
-    self.my_plant = None
-    self.mode = 1
-    self.target_range = random.randrange(50,1000)
-    pass
+    def __init__(self, junk):
+        self.my_plant = None
+        self.mode = 1
+        self.target_range = random.randrange(50,1000)
+        pass
 
-  def length(self,a,b):
-      return int(math.sqrt((a*a)+(b*b)))
-  
-  def act(self,view,msg):
-    x_sum = 0
-    y_sum = 0
-    dir = 1
-    n = len(view.get_plants())
-    me = view.get_me()
-    mp = (mx,my)= me.get_pos()
+    def length(self,a,b):
+        return int(math.sqrt((a*a)+(b*b)))
 
-    for a in view.get_agents():
-      if (a.get_team()!=me.get_team()):
-        return cells.Action(cells.ACT_ATTACK,a.get_pos())
+    def act(self,view,msg):
+        x_sum = 0
+        y_sum = 0
+        dir = 1
+        n = len(view.get_plants())
+        me = view.get_me()
+        mp = (mx,my)= me.get_pos()
 
-    if(n>0):
-      if (not self.my_plant):
-        self.my_plant = view.get_plants()[0]
-      elif self.my_plant.get_eff()<view.get_plants()[0].get_eff():
-        self.my_plant = view.get_plants()[0]
+        for a in view.get_agents():
+            if (a.get_team()!=me.get_team()):
+                return cells.Action(cells.ACT_ATTACK,a.get_pos())
 
-    if (((me.energy < self.target_range) and (view.get_energy().get(mx, my) > 0)) 
-        or (view.get_energy().get(mx, my) > 100)) :
-      return cells.Action(cells.ACT_EAT)
+        if(n>0):
+            if (not self.my_plant):
+                self.my_plant = view.get_plants()[0]
+            elif self.my_plant.get_eff()<view.get_plants()[0].get_eff():
+                self.my_plant = view.get_plants()[0]
 
-    if self.my_plant:
-      dist = self.length(abs(mx-self.my_plant.get_pos()[0]),abs(my-self.my_plant.get_pos()[1]))
-      if (not view.get_me().loaded) and ((dist%5>0)or(abs(mx-self.my_plant.get_pos()[0])<2)) and (random.random()>0.5):
-        return cells.Action(cells.ACT_LIFT)
-      if (view.get_me().loaded) and ((dist%5 == 0) and (abs(mx-self.my_plant.get_pos()[0])>=2)):
-        return cells.Action(cells.ACT_DROP)
-      if view.get_me().energy < dist*1.5:
-        (mx,my) = self.my_plant.get_pos()
-        return cells.Action(cells.ACT_MOVE,(mx+random.randrange(-1,2),my+random.randrange(-1,2)))
+        if (((me.energy < self.target_range) and (view.get_energy().get(mx, my) > 0))
+            or (view.get_energy().get(mx, my) > 100)) :
+            return cells.Action(cells.ACT_EAT)
 
-    if (random.random()>0.9):
-      return cells.Action(cells.ACT_SPAWN,(mx+random.randrange(-1,2),my+random.randrange(-1,2)))
-    else:
-      return cells.Action(cells.ACT_MOVE,(mx+random.randrange(-1,2),my+random.randrange(-1,2)))
+        if self.my_plant:
+            dist = self.length(abs(mx-self.my_plant.get_pos()[0]),abs(my-self.my_plant.get_pos()[1]))
+            if (not view.get_me().loaded) and ((dist%5>0)or(abs(mx-self.my_plant.get_pos()[0])<2)) and (random.random()>0.5):
+                return cells.Action(cells.ACT_LIFT)
+            if (view.get_me().loaded) and ((dist%5 == 0) and (abs(mx-self.my_plant.get_pos()[0])>=2)):
+                return cells.Action(cells.ACT_DROP)
+            if view.get_me().energy < dist*1.5:
+                (mx,my) = self.my_plant.get_pos()
+                return cells.Action(cells.ACT_MOVE,(mx+random.randrange(-1,2),my+random.randrange(-1,2)))
+
+        if (random.random()>0.9):
+            return cells.Action(cells.ACT_SPAWN,(mx+random.randrange(-1,2),my+random.randrange(-1,2)))
+        else:
+            return cells.Action(cells.ACT_MOVE,(mx+random.randrange(-1,2),my+random.randrange(-1,2)))
